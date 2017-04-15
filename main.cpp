422d7599388bd4d885080dbab85de8594f335892
trincon <trincon@tampabay.rr.com>
2017-04-15 16:28:31 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,7 +9,10 @@
 #include "player.h"
 #include "bullet.h"
 #include "gameloop.h"
+#include "game.h"
+#include "mainMenu.h"
 
+Game *game;
 gameLoop * globalGame;
 
 int main(int argc, char *argv[])
@@ -17,6 +20,12 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
 
     globalGame = new gameLoop(1920, 1080);
+    
+    game = new Game();
+    
+    game->show();
+    
+    game->displayMainMenu();
 
     return a.exec();
 
