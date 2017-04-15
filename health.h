2e8616aa1a9796b060307b68fc38ac6fbcd459bf
trincon <trincon@tampabay.rr.com>
2017-04-14 21:17:41 -0400

    Update health.h

--- health.h
+++ health.h
@@ -7,6 +7,7 @@ class Health: public QGraphicsTextItem{
 public:
     Health(QGraphicsItem * parent=0);
     void decrease();
+    void resetHealth();
     int getHealth();
 private:
     int health;
