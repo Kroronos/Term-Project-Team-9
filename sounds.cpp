aa602faa414aa0192bca8ce492e26b82a2abd506
Jesse Martinez <astroshadow@bellsouth.net>
2017-04-17 06:21:03 -0400

    Add files via upload

--- sounds.cpp
+++ sounds.cpp
@@ -7,12 +7,12 @@
 
 void sounds::playBullet()
 {
-    QSound::play(":/sounds/sounds/laser.aiff");
+    QSound::play(":/sounds/sounds/laser.wav");
 }
 
 void sounds::playExplosion()
 {
-    QSound::play(":/sounds/sounds/explosion.mp3");
+    QSound::play(":/sounds/sounds/explosion.wav");
 }
 
 void sounds::playRound(int round)
@@ -20,27 +20,27 @@ void sounds::playRound(int round)
     switch (round) {
 
         case 1:
-            QSound::play(":/sounds/sounds/Visager_-_05_-_Battle.mp3");
+            QSound::play(":/sounds/sounds/Visager_-_05_-_Battle.wav");
             break;
         case 2:
             //QSound::stop();
-            QSound::play(":/sounds/sounds/Captive_Portal_-_06_-_Is_It_Minus_5.mp3");
+            QSound::play(":/sounds/sounds/Captive_Portal_-_06_-_Is_It_Minus_5.wav");
             break;
         case 3:
             //QSound::stop();
-            QSound::play(":/sounds/sounds/Visager_-_01_-_Title_Theme.mp3");
+            QSound::play(":/sounds/sounds/Visager_-_01_-_Title_Theme.wav");
             break;
         case 4:
             //QSound::stop();
-            QSound::play(":/sounds/sounds/GoCart - Drop Mix.mp3");
+            QSound::play(":/sounds/sounds/GoCart - Drop Mix.wav");
             break;
         case 5:
             //QSound::stop();
-            QSound::play(":/sounds/sounds/Azureflux_-_04_-_I_Ate_All_The_Snacks.mp3");
+            QSound::play(":/sounds/sounds/Azureflux_-_04_-_I_Ate_All_The_Snacks.wav");
             break;
         case 6:
             //QSound::stop();
-            QSound::play(":/sounds/sounds/Visager_-_08_-_Winter_Village.mp3");
+            QSound::play(":/sounds/sounds/Visager_-_08_-_Winter_Village.wav");
             break;
 
 
